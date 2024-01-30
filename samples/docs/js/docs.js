b8212a119a63cf2533aebf5355ffb2d0e856cfb2
Abhishek Bhoyar <117527484+anon576@users.noreply.github.com>
2024-01-30 21:08:53 +0530

    Update docs.js

--- samples/docs/js/docs.js
+++ samples/docs/js/docs.js
@@ -21,7 +21,7 @@ function addPage(page, book) {
 	var element = $('<div />', {});
 
 	if (book.turn('addPage', element, page)) {
-		if (page<60) {
+		if (page<64) {
 			element.html('<div class="gradient"></div><div class="loader"></div>');
 			loadPage(page);
 		}
